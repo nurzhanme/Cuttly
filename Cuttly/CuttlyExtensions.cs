a227469f5b1b1ee9351238170505126924682663
Nurzhan <nurzhait@gmail.com>
2022-03-28 16:08:00 +0600

    add extensions

---
+++ Cuttly/CuttlyExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cuttly
+{
+    public static class CuttlyExtensions
+    {
+        public static void AddCuttly(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CuttlyOptions>(configuration);
+        }
+
+        public static void AddCuttly(this IServiceCollection services, Action<CuttlyOptions> options)
+        {
+            services.Configure(options);
+        }
+    }
+}
